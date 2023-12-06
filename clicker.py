d0e03d72ea0b06a05d0b19bd64b4307923749915
VeiledEnigma <lennartbenninghaus@googlemail.com>
2023-12-06 22:22:55 +0100

    first commit

---
+++ clicker.py
@@ -0,0 +1,67 @@
+import asyncio
+import keyboard
+
+import pydirectinput
+
+
+class Clicker:
+
+    def __init__(self) -> None:
+        self.hotkey = "F20"
+        self.mouse_button = "left"
+        # Bool for the start button
+        self.running = False
+        # Bool for the press once mode
+        self.press_mode_running = False
+        # Pause in seconds for pydirectinput.PAUSE
+        self.pause = 0.05
+        # Only the first letter matters - either "H" or "P"
+        self.mode = "Hold down"
+
+    def set_mouse_button(self, mouse_button: str) -> None:
+        self.mouse_button = mouse_button.lower()
+
+    def set_hotkey(self, hotkey: str) -> None:
+        self.hotkey = hotkey
+
+    def set_clicks_per_second(self, clicks_per_second: int) -> None:
+        """Takes the CPS (int) from the slider as input. Converts it into seconds (float)
+        The calculated value does not translate exactly into the right amount of seconds
+        The amplifier of 4% is used to make up for that difference"""
+        self.pause = 1/(clicks_per_second*1.04)
+
+    def set_mode(self, mode: str) -> None:
+        self.mode = mode
+
+    async def start(self) -> None:
+        """Start button"""
+        self.running = True
+        if self.mode.startswith("P"):
+            keyboard.add_hotkey(self.hotkey, self.switch_press_mode)
+        await self.autoclick()
+
+    def stop(self) -> None:
+        """Stop button"""
+        self.running = False
+
+    def switch_press_mode(self) -> None:
+        """Used to as callback function for keyboard.add_hotkey"""
+        self.press_mode_running = not self.press_mode_running
+
+    async def autoclick(self) -> None:
+        """Coroutine to wait for inputs and execute the mouse clicks alongside the UI"""
+        pydirectinput.PAUSE = self.pause
+
+        if self.mode.startswith("H"):
+            while self.running:
+                while keyboard.is_pressed(self.hotkey):
+                    pydirectinput.click(button=self.mouse_button)
+                    await asyncio.sleep(0)
+                await asyncio.sleep(0.1)
+
+        if self.mode.startswith("P"):
+            while self.running:
+                while self.press_mode_running:
+                    pydirectinput.click(button=self.mouse_button)
+                    await asyncio.sleep(0)
+                await asyncio.sleep(0.1)
---
+++ main.py
@@ -0,0 +1,105 @@
+from clicker import Clicker
+import keyboard
+from nicegui import app, ui
+from nicegui.events import KeyEventArguments
+
+clicker = Clicker()
+
+
+def set_hotkey(hotkey_button: ui.button, hotkey_label: ui.label) -> None:
+    """Function to safe the pressed Key Combination and assign the Hotkey to the Clicker
+    The UI Button and Label are also updated within this function"""
+
+    def safe_key(key: KeyEventArguments):
+        """Callback function for the Keyboard Listener"""
+        # Save the pressed key
+        keys_pressed.append(key)
+        # If a key is lifted start evaluating the input
+        if key.action.keyup:
+            hotkey_list = []
+            for hotkey in keys_pressed:
+                # Get  string representation of pressed key
+                hotkey_str = str(hotkey.key)
+                # Modify the string for output
+                hotkey_str = hotkey_str.upper() if len(hotkey_str) == 1 else hotkey_str
+                # A set comprehension does not work correctly, because it does not preserve the order
+                if not hotkey.action.keyup and hotkey_str not in hotkey_list:
+                    hotkey_list.append(hotkey_str)
+
+            # String representation of the combination
+            hotkey = " + ".join([hotkey.lower() for hotkey in hotkey_list])
+            clicker.hotkey = hotkey
+            keyboard_listener.active = False
+            hotkey_label.set_text(" + ".join(hotkey_list))
+            hotkey_button.enable()
+            hotkey_button.set_text("Change Hotkey")
+
+    keys_pressed = []
+    hotkey_button.disable()
+    hotkey_button.set_text("Waiting for input...")
+    keyboard_listener = ui.keyboard(on_key=lambda e: safe_key(e))
+
+
+def main():
+    """The whole UI part could be outsourced to a different UI-State Class. But it works for now"""
+    app.native.window_args['resizable'] = False
+    app.native.start_args['debug'] = False
+
+    ui.dark_mode().enable()
+
+    with ui.row().classes("w-full no-wrap"):
+        with ui.card().classes("w-1/2 items-center h-32"):
+            ui.label("Mouse Button").classes("font-bold")
+            button_selection = ui.toggle(["Left", "Middle", "Right"],
+                                         value="Left",
+                                         on_change=lambda e: clicker.set_mouse_button(e.value))
+
+        with ui.card().classes("w-1/2 items-center h-32"):
+            ui.label("Clicks per second").classes("font-bold")
+            slider = ui.slider(value=20,
+                               min=1,
+                               max=100,
+                               step=1,
+                               on_change=lambda e: clicker.set_clicks_per_second(int(e.value))).classes("items-center")
+            ui.label().bind_text_from(slider, "value")
+
+    with ui.row().classes("w-full no-wrap"):
+        with ui.card().classes("w-1/2 items-center h-32"):
+            ui.label("Mode").classes("font-bold")
+            mode_selection = ui.toggle(["Hold down", "Press once"],
+                                       value="Hold down",
+                                       on_change=lambda e: clicker.set_mode(e.value))
+
+        with ui.card().classes("w-1/2 items-center h-32"):
+            hotkey_label = ui.label("F20").classes("font-bold")
+            hotkey_button = ui.button("Change Hotkey", on_click=lambda: set_hotkey(hotkey_button, hotkey_label))
+    # ui.row()
+    # ui.row()
+
+    with ui.row().classes("w-full no-wrap"):
+        async def on_start() -> None:
+            start_button.disable()
+            slider.disable()
+            button_selection.disable()
+            mode_selection.disable()
+            hotkey_button.disable()
+            start_button.set_text("Waiting for Hotkey...")
+            await clicker.start()
+
+        def on_stop() -> None:
+            clicker.stop()
+            start_button.enable()
+            start_button.set_text("Start")
+            slider.enable()
+            button_selection.enable()
+            mode_selection.enable()
+            hotkey_button.enable()
+
+        start_button = ui.button("Start", on_click=on_start).classes("w-3/4")
+        ui.button("Stop", on_click=on_stop).classes("w-1/4")
+
+    ui.run(native=True, window_size=(700, 400), reload=False, title="Clicky")
+
+
+if __name__ in ("__main__", "__mp_main__"):
+    main()
